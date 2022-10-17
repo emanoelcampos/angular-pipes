01f713a45b27b461e119db78ffd060d3563d9bb4
emanoelcampos <emanoel.cv@gmail.com>
2022-10-17 17:35:11 -0300

    Async

--- src/app/exemplos-pipes/exemplos-pipes.component.ts
+++ src/app/exemplos-pipes/exemplos-pipes.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, interval } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-exemplos-pipes',
@@ -34,6 +36,13 @@ export class ExemplosPipesComponent implements OnInit {
    );
 
   }
+
+  valorAsync = new Promise((resolve, reject) => {
+    setTimeout(() => resolve('Valor assíncrono'), 2000)
+  });
+
+  valorAsync2 = interval(2000).pipe(
+    map(valor => 'Valor assíncrono 2'));
 
   constructor() { }
 
